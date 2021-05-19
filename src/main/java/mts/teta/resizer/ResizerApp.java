4f9a484cea1ad263ce783cd5cb52838bf0c5a6c2
antongrutsin <Risenka13524>
2021-05-19 12:04:20 +0300

    Done

--- src/main/java/mts/teta/resizer/ResizerApp.java
+++ src/main/java/mts/teta/resizer/ResizerApp.java
@@ -1,12 +1,28 @@
 package mts.teta.resizer;
 
+import mts.teta.resizer.imageprocessor.BadAttributesException;
+import mts.teta.resizer.imageprocessor.ImageProcessor;
+import mts.teta.resizer.util.ConsoleAttributes;
 import picocli.CommandLine;
+import picocli.CommandLine.*;
 
+import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "resizer", mixinStandardHelpOptions = true, version = "resizer 0.0.1", description = "...")
+@Command(name = "convert",
+        mixinStandardHelpOptions = true,
+        version = "convert 0.0.1",
+        description = "...",
+        sortOptions = false,
+        headerHeading = "Usage:%n%n",
+        synopsisHeading = "%n",
+        descriptionHeading = "%nDescription:%n%n",
+        parameterListHeading = "%nParameters:%n",
+        optionListHeading = "%nOptions:%n")
 public class ResizerApp extends ConsoleAttributes implements Callable<Integer> {
+
     public static void main(String... args) {
         int exitCode = runConsole(args);
         System.exit(exitCode);
@@ -17,9 +33,13 @@ public class ResizerApp extends ConsoleAttributes implements Callable<Integer> {
     }
 
     @Override
-    public Integer call() throws Exception {
+    public Integer call() throws IOException, BadAttributesException {
         ImageProcessor imageProcessor = new ImageProcessor();
-        imageProcessor.processImage(ImageIO.read(inputFile), this);
+        try {
+            imageProcessor.processImage(ImageIO.read(getInputFile()), this);
+        } catch (IIOException e){
+            throw new IIOException("Can't read input file!");
+        }
         return 0;
     }
 }
